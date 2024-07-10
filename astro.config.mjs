f4f65e37a33492269c1568987eb966931baa0cc8
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-10 13:56:57 +0330

    conf(astro.config.mjs): Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
+  build: {
+    assets: "assets",
+  },
   integrations: [tailwind(), react()]
 });
