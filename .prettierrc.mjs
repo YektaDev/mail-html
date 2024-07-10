18b0ced4879cdb8ce586ae239ba4c8ff243f447d
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-10 13:56:42 +0330

    conf(Prettier): Update config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,15 @@
 /** @type {import("prettier").Config} */
 export default {
-    plugins: ['prettier-plugin-astro'],
+    "arrowParens": "avoid",
+    "semi": true,
+    "tabWidth": 2,
+    "printWidth": 100,
+    "singleQuote": false,
+    "jsxSingleQuote": false,
+    "trailingComma": "all",
+    "bracketSpacing": true,
+    "endOfLine": "lf",
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
     overrides: [
         {
             files: '*.astro',
