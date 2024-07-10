dcbf36285f27ce982dea9be087a717269183df95
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-10 17:46:49 +0330

    nice: Add primary color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,14 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
+            colors: {
+                primary: colors.gray,
+            },
             fontFamily: {
                 sans: ['"Overpass Variable"', ...defaultTheme.fontFamily.sans],
             },
