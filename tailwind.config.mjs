85d66bf80b7fd656bc4966ad7c885fb7c8c077cd
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-10 17:02:24 +0330

    nice: Add custom font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Overpass Variable"', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
 }
