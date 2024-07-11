df783ff42a607e3681ce614e11bc47e6451d5b24
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-11 22:57:11 +0330

    conf: Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mailhtml.yekta.dev",
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "hover",
