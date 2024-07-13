10121e3de528213bf9b84785c0de6aa8ad267373
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-13 19:28:58 +0330

    feat(fieldStyle): Add

---
+++ src/components/fieldStyle.ts
@@ -0,0 +1,4 @@
+const fieldStyle =
+  "text-xs rounded border border-primary-300 placeholder-primary-600 placeholder-opacity-70 focus:outline-none focus:ring-1 focus:ring-primary-400";
+
+export default fieldStyle;
