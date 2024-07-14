61592fa04a0a5d2a0f311a2c28b6c6e9ba7aa862
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-14 06:03:55 +0330

    feat: Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mailhtml.yekta.dev",
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: "hover",
+    defaultStrategy: "hover"
   },
   build: {
-    assets: "assets",
+    assets: "assets"
   },
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), robotsTxt()]
 });
