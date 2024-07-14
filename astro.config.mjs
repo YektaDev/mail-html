3e94556cd1d6c58d4816cb7710e26a49c20119bb
Ali Khaleqi Yekta <me@yekta.dev>
2024-07-14 23:07:32 +0330

    feat: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +14,5 @@ export default defineConfig({
   build: {
     assets: "assets"
   },
-  integrations: [tailwind(), react(), robotsTxt()]
+  integrations: [tailwind(), react(), robotsTxt(), sitemap()]
 });
